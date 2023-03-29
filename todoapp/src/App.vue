3872cd92a33acfc85a5cbd9f419e4ff999af27fe
YuukiTerui <teriyaki0839@gmail.com>
2023-03-30 00:33:33 +0900

    画面横幅を小さくしたときにタイトルの位置が中央に来るよう見た目を調整

--- todoapp/src/App.vue
+++ todoapp/src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" />
-    <div class="wrapper">
-
-      <h1>TodoAppWithVue</h1>
-    </div>
+    <h1 id="title">TodoAppWithVue</h1>
   </header>
 
   <main class="main-content">
@@ -74,11 +71,17 @@ header {
   line-height: 1.5;
 }
 
+#title {
+  display: block;
+  text-align: center;
+  margin: 0 auto;
+}
+
 .logo {
   display: block;
-  margin: 0 auto 2rem;
-  width: 64px;
-  height: 64px;
+  margin: 0 auto;
+  width: 48px;
+  height: 48px;
 }
 
 @media (min-width: 800px) {
@@ -92,10 +95,8 @@ header {
     margin: 0 2rem 0 0;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  #title {
+    margin: 0 0;
   }
 }
 </style>
