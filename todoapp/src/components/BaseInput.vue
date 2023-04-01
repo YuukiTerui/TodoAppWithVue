076926f27c62f3e84b2829b4553a84d3d46dbe3a
YuukiTerui <teriyaki0839@gmail.com>
2023-04-02 01:36:41 +0900

    Create BaseInput.vue

---
+++ todoapp/src/components/BaseInput.vue
@@ -0,0 +1,30 @@
+<template>
+    <input 
+        :type="type"
+        class="BaseInput"
+        :class="classes"
+        :disabled="disabled || loading"
+        :placeholder="placeholder"
+    />
+</template>
+
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+interface Props {
+    type: string,
+    placeholder: string,
+    loading:boolean,
+    disabled: boolean,
+}
+const props = defineProps<Props>();
+
+const classes = computed(() => {
+    return ["_" + props.type];
+});
+</script>
+
+
+<style scoped>
+</style>
